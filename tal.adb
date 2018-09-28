be5c07be5b872d513b1e61e408b69ac809cad4b6
Felstj <31950479+Felstj@users.noreply.github.com>
2018-09-28 16:08:11 +0200

    Add files via upload

---
+++ tal.adb
@@ -0,0 +1,28 @@
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+with Ada.Text_Io; use Ada.Text_Io;
+with eye;
+with random_character;
+
+
+procedure Tal is
+A:Integer:=1;   
+B:Integer;  
+   
+begin
+   
+   
+   Put("Skirv in ett heltal");
+   Get(B);
+   
+   Eye(A);
+   
+   New_Line;
+   
+   random_character(B);
+   
+   
+   
+end Tal;
+  
+# TDIU08
